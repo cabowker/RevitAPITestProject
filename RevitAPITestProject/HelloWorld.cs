37e5ade2a840f2b8aaa29578aeca1eba4a7e117f
cbowke <chris@valorvdc.com>
2025-01-11 10:01:01 -0500

    added manifest file

---
+++ RevitAPITestProject/HelloWorld.cs
@@ -0,0 +1,15 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace RevitAPITestProject;
+
+[Transaction(TransactionMode.Manual)]
+public class HelloWorld : IExternalCommand
+{
+    public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+    {
+        TaskDialog.Show("Hello", "Hello World");
+        return Result.Succeeded;
+    }
+}
